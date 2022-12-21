4062783dfa962751e71195c1b82d0d614ed9eb48
1alberto994 <alby94ct@gmail.com>
2022-12-21 14:41:01 +0100

    commenti

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let x=100 % 3
+for(let i=1; i<=100;i++){
+    console.log(i)
+   if(x==0){
+    console.log("scrivo buzz")
+   }
+//    il mio ragionamento è impostare gli elemti da 1 a 100    fatto
+//     imporre una condizione se sono multipli di 3 io scrivo buzz
+//     se sono multipli di 3 && di 5 io scrivo fizzbuzz
+//     se sono multipli di 5 io scrivo fizz
+    
+    
+}
