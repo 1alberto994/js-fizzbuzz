a74e76ea23e1fd87be93dae9d2408c315119f6af
1alberto994 <alby94ct@gmail.com>
2022-12-21 15:03:40 +0100

    calcolo dei multipli

--- scripts.js
+++ scripts.js
@@ -1,13 +1,24 @@
-let x=100 % 3
-for(let i=1; i<=100;i++){
-    console.log(i)
-   if(x==0){
-    console.log("scrivo buzz")
-   }
+n=100 % 3
+nDue=100 % 5
+nVal=n.value
+nDue=nDue.value
+nDueVal=nDue   
+for(let i=0 ; i<=100;i++){
+    if(nVal==n){
+        console.log("scrivo buzz")
+    }
+    // else if(multipli 3 && multi 5 ){
+//  console.log("scrivo fizzbuzz")
+    // }
+    // else if(multi 5){
+// console.log("scrivo fizz")
+    // }
+}
+    
+    
 //    il mio ragionamento è impostare gli elemti da 1 a 100    fatto
 //     imporre una condizione se sono multipli di 3 io scrivo buzz
 //     se sono multipli di 3 && di 5 io scrivo fizzbuzz
 //     se sono multipli di 5 io scrivo fizz
     
     
-}
